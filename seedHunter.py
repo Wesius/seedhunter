09a80df2860ad237c0e02ea8a643ec455ddbeabd
BuildTools <unconfigured@null.spigotmc.org>
2022-09-29 18:25:02 -0400

    Changes

--- seedHunter.py
+++ seedHunter.py
@@ -6,6 +6,8 @@ import sys
 import random
 import os
 
+# TODO Score counter
+
 # Colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -130,26 +132,8 @@ pygame.display.flip()
 pygame.time.delay(1500)
 
 startScreen = Background('data/startScreen.png', [0, 0])
-while gameStarted == False:
-    screen.blit(startScreen.image,
-                (startScreen.rect.left, startScreen.rect.top))
-    pygame.draw.rect(screen, PINK, Rect(225, 350, 250, 100))
-    screen.blit(startText, (280, 350))
-    pygame.display.flip()
-    events = pygame.event.get()
-    for event in events:
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouseX = pygame.mouse.get_pos()[0]
-            mouseY = pygame.mouse.get_pos()[1]
-            if mouseX > 225 and mouseX < 475 and mouseY > 350 and mouseY < 450:
-                gameStarted = True
+
+
 # Initializing player
 p = Player()
 
@@ -159,89 +143,112 @@ p = Player()
 
 p.y_coord -= 500
 
+mouseX = 0
+mouseY = 0
+
+
 # Initializing backgrounds
-# FIXME Gotta fix the fact that backgrounds vanish when player goes too far down
 bg = [Background('data/bg.png', [0, 0]), Background('data/bg.png',
-                                                    [0, -500]), Background('data/bg.png', [0, -1000])]
+                                                    [0, -500]), Background('data/bg.png', [0, -1000]), Background('data/bg.png', [0, 500]), Background('data/bg.png', [0, 1000])]
 # Main game loop
 while not done:
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
-            done = True
+            pygame.quit()
+            sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                done = True
+                pygame.quit()
+                sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN and gameStarted == False:
+            mouseX = pygame.mouse.get_pos()[0]
+            mouseY = pygame.mouse.get_pos()[1]
+            if mouseX > 225 and mouseX < 475 and mouseY > 350 and mouseY < 450:
+                gameStarted = True
 
-    # Changing camera offset
-    camOffset = -p.y_coord + screen.get_height()/2
+    if gameStarted == True:
+        # Changing camera offset
+        camOffset = -p.y_coord + screen.get_height()/2
 
-    # Player functions
-    p.keyCheck(events)
-    p.move(screen)
+        # Player functions
+        p.keyCheck(events)
+        p.move(screen)
 
-    # Background procesing
-    if gameOver == False:
-        screen.fill((35, 206, 235))
+        # Background procesing
+        if gameOver == False:
+            screen.fill((35, 206, 235))
 
-    # Blit
-    [screen.blit(back.image, (back.rect.left, back.rect.top + camOffset))
-     for back in bg]
-    # Background processing
-    for back in bg:
-        if back.rect.top > p.y_coord + 250:
-            back.rect.top -= 1000
+        # Blit
+        [screen.blit(back.image, (back.rect.left, back.rect.top + camOffset))
+         for back in bg]
+        # Background processing
+        for back in bg:
+            if back.rect.top > p.y_coord + 250:
+                back.rect.top -= 1000
+            if back.rect.bottom < p.y_coord - 250:
+                back.rect.bottom += 1000
 
-    # Removes chocolates below the player
-    chocolateLst = [
-        chocolate for chocolate in chocolateLst if not p.y_coord < chocolate.y_coord - 300]
+        # Removes chocolates below the player
+        chocolateLst = [
+            chocolate for chocolate in chocolateLst if not p.y_coord < chocolate.y_coord - 300]
 
-    # Removes seeds that are colliding with player and seeds far below the player
-    seedLst = [seed for seed in seedLst if not p.Rect.colliderect(seed.Rect)]
-    seedLst = [seed for seed in seedLst if not p.y_coord < seed.y_coord - 300]
+        # Removes seeds that are colliding with player and seeds far below the player
+        seedLst = [
+            seed for seed in seedLst if not p.Rect.colliderect(seed.Rect)]
+        seedLst = [seed for seed in seedLst if not p.y_coord <
+                   seed.y_coord - 300]
 
-    if len(chocolateLst) < difficulty:
-        chocolateLst.append(Chocolate(int(p.y_coord)))
+        if len(chocolateLst) < difficulty:
+            chocolateLst.append(Chocolate(int(p.y_coord)))
 
-    if len(seedLst) < INITIAL_SEED_COUNT:
-        seedLst.append(Seed(int(p.y_coord)))
-        difficulty += 1
+        if len(seedLst) < INITIAL_SEED_COUNT:
+            seedLst.append(Seed(int(p.y_coord)))
+            difficulty += 1
 
-    for chocolate in chocolateLst:
-        if p.Rect.colliderect(chocolate.rect):
+        if gameOver == True:
+            p.x_coord = 1000
+            p.y_coord = -1000
+            p.kill()
+            for chocolate in chocolateLst:
+                chocolate.kill()
+            for seed in seedLst:
+                seed.kill()
+            for b in bg:
+                b.kill()
+            screen.blit(gameOverText, (screen.get_width() //
+                        2 - 150, screen.get_height()//2 - 50))
+            pygame.display.flip()
+            pygame.time.delay(3000)
+            done = True
+            # TODO Go back to start screen
+
+        [screen.blit(chocolate.image, (chocolate.x_coord, chocolate.y_coord + camOffset))
+         for chocolate in chocolateLst]
+        [screen.blit(seed.image, (seed.x_coord, seed.y_coord + camOffset))
+         for seed in seedLst]
+        screen.blit(p.image, (p.x_coord, p.y_coord + camOffset))
+
+        # Two gameover conditions
+        for chocolate in chocolateLst:
+            if p.Rect.colliderect(chocolate.rect):
+                pygame.mixer.Sound.play(gameOverSound)
+                gameOver = True
+
+        if p.y_coord > screen.get_height() + camOffset:
             pygame.mixer.Sound.play(gameOverSound)
+            p.x_coord = 10000
+            p.y_coord = -10000
             gameOver = True
 
-    if gameOver == True:
-        p.x_coord = 1000
-        p.y_coord = -1000
-        p.kill()
-        for chocolate in chocolateLst:
-            chocolate.kill()
-        for seed in seedLst:
-            seed.kill()
-        screen.blit(gameOverText, (screen.get_width() //
-                    2 - 150, screen.get_height()//2 - 50))
-        pygame.display.flip()
-        # pygame.time.delay(5000)
-        # TODO Go back to start screen
-
-    [screen.blit(chocolate.image, (chocolate.x_coord, chocolate.y_coord + camOffset))
-     for chocolate in chocolateLst]
-    [screen.blit(seed.image, (seed.x_coord, seed.y_coord + camOffset))
-     for seed in seedLst]
-    screen.blit(p.image, (p.x_coord, p.y_coord + camOffset))
-
-    if p.y_coord > screen.get_height() + camOffset:
-        pygame.mixer.Sound.play(gameOverSound)
-        p.x_coord = 10000
-        p.y_coord = -10000
-        gameOver = True
+    else:
+        screen.blit(startScreen.image,
+                    (startScreen.rect.left, startScreen.rect.top))
+        pygame.draw.rect(screen, PINK, Rect(225, 350, 250, 100))
+        screen.blit(startText, (280, 350))
 
     pygame.display.flip()
     clock.tick(60)
-
-    gitTest = 10
 
 pygame.quit()
 sys.exit()
